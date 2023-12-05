1dcf9e486c80de2199ba8946b80907629260270f
HardCodeMatter <dklymovets1@gmail.com>
2023-12-05 22:15:47 +0200

    Updated application, added division function.

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.example.debugging
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        logging();
+        Log.d(TAG, "this is where the app crashed before")
+        val helloTextView: TextView = findViewById(R.id.hello_world)
+        Log.d(TAG, "this should be logged if the bug is fixed")
+        helloTextView.text = "Hello, debugging!"
+        logging()
+        division()
     }
 
-    // Logging Function
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(4) {
+            Thread.sleep(3000)
+            findViewById<TextView>(R.id.hello_world).setText("${numerator / denominator}")
+            denominator--
+        }
+    }
+
     fun logging() {
         Log.e(TAG, "ERROR: a serious error like an app crash")
         Log.w(TAG, "WARN: warns about the potential for serious errors")
